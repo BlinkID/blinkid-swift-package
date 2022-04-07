c823f8858de7f8a8ebfd307ddd4288df942c8817
Mijo Gracanin <mijo.gracanin@gmail.com>
2022-04-07 16:31:32 -0400

    Update for v5.16.1

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.4
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Microblink",
-            url: "https://github.com/BlinkID/blinkid-ios/releases/download/v5.16.0/Microblink.xcframework.zip",
-            checksum: "45abb73a59089bb1902227df59729c3324e534ce580da61e3410c0927fa5a18a")
+            url: "https://github.com/BlinkID/blinkid-ios/releases/download/v5.16.1/Microblink.xcframework.zip",
+            checksum: "34497041dff3a215087bfceff275c5fabf88ae87347e708472365d713a733397")
     ]
 )
 
